c7137d22b4d2f51ddef9c2b8b86bb1323d599700
Lakshika-Swarnamali <swarnamalial@gmail.com>
2019-01-27 20:55:19 +0530

    changed job post controller

--- src/main/java/com/example/login/LoginExample/Controller/JobTypeController.java
+++ src/main/java/com/example/login/LoginExample/Controller/JobTypeController.java
@@ -14,14 +14,26 @@ public class JobTypeController {
     public JobTypeController(JobTypeRepository jobTypeRepository){
         this.jobTypeRepository=jobTypeRepository;
     }
+
     @RequestMapping(method = RequestMethod.POST)
     public JobType createJobType(@RequestBody JobType jobType) {
         return jobTypeRepository.save(jobType);
     }
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public Boolean deleteJobType( @PathVariable long id) {
+        try{
+            this.jobTypeRepository.deleteById(id);
+            return true;
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
     @RequestMapping(value = "/all", method = RequestMethod.GET)
-    //   @CrossOrigin(origins = "http://localhost:4200")
     public @ResponseBody
     Iterable<JobType> getAllJobTypes() {
         return jobTypeRepository.findAll();
     }
+
 }
--- src/main/java/com/example/login/LoginExample/Models/Complain.java
+++ src/main/java/com/example/login/LoginExample/Models/Complain.java
@@ -1,11 +1,16 @@
 package com.example.login.LoginExample.Models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+@Getter // Automatically getter and setter method using Lombok
+@Setter
 
 @Entity
 @Table(name = "complain")
@@ -14,68 +19,13 @@ public class Complain {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
-    public String getComplain() {
-        return complain;
-    }
-
-    public void setComplain(String complain) {
-        this.complain = complain;
-    }
-
-
     private String complain;
-
-
-
-   private  LocalDateTime dt;
-   private long toWhoom;
-
-    public LocalDateTime getDt() {
-        return dt;
-    }
-
-    public void setDt(LocalDateTime dt) {
-        this.dt = dt;
-    }
-
-    public long getToWhoom() {
-        return toWhoom;
-    }
-
-    public void setToWhoom(long toWhoom) {
-        this.toWhoom = toWhoom;
-    }
-
-    public long getComplainer() {
-        return complainer;
-    }
-
-    public void setComplainer(long complainer) {
-        this.complainer = complainer;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public long getJobid() {
-        return jobid;
-    }
-
-    public void setJobid(long jobid) {
-        this.jobid = jobid;
-    }
-
+    private  LocalDateTime dt;
+    private long toWhoom;
     private long complainer;
-   private String city;
-   private long jobid;
-
-
+    private String city;
+    private long jobid;
+    private String adminReply;
 
     public Complain() {
 
